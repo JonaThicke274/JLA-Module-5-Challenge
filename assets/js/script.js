139d14751ffc52682f81abb8afc9ad5517be49f1
JonaThicke274 <lipatajonathan274@gmail.com>
2022-08-26 00:25:49 -0400

    html: removed div for 8am; js: added code to fetch current date and display it on document/page, added code to iterate through each time block class and compare them against the current hour for color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,35 @@
 // Assigns current day, date, and local time to a variable
-var currentDate = moment().format("LLLL")
+var currentDate = moment().format("LLLL");
 // Dynamically makes the html content of the id #currentDay as the variable with the current dates info
-$("#currentDay").html(currentDate)
+$("#currentDay").html(currentDate);
 
 // Iterate through the time blocks and apply color-code
 var checkTimeBlocks = function() {
     // Assigns current hour to variable to check against
-    currentHour = moment().hour()
-}
-// Call/get info from local storage to populate textarea elements
+    currentHour = moment().hour();
+    
+    // Function for iterating through each time block to color-code
+    $(".time-block").each(function (){
+        // Assign variable to check against currentHour based on id
+        hourBlock = parseInt($(this).attr("id").split("time")[1]);
+        // Assigns class for color coding
+        if (hourBlock < currentHour) {
+            $(this).addClass("past");
+        }
+        else if (hourBlock === currentHour) {
+            $(this).addClass("present");
+        }
+        else if (hourBlock > currentHour) {
+            $(this).addClass("future");
+        }
+    });
+};
+
+// Function for saving tasks in each time block
+
+// Load info from localStorage to populate saved tasks/text
 
 // Clear localstorage if it is a new date?
+
+
+checkTimeBlocks();
