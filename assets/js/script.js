6e314d2fbdc2d668e36d858cb462f3ecbe31777c
JonaThicke274 <lipatajonathan274@gmail.com>
2022-08-25 23:57:12 -0400

    started function for checking time blocks against current time(specifically hour), changed ids of time blocks in html to reflect their 24hr format so moment can be used to check which color code to apply

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ var currentDate = moment().format("LLLL")
 // Dynamically makes the html content of the id #currentDay as the variable with the current dates info
 $("#currentDay").html(currentDate)
 
-// Iterate through the time blocks and apply color-code if it's past, present, or future
-
+// Iterate through the time blocks and apply color-code
+var checkTimeBlocks = function() {
+    // Assigns current hour to variable to check against
+    currentHour = moment().hour()
+}
 // Call/get info from local storage to populate textarea elements
 
 // Clear localstorage if it is a new date?
